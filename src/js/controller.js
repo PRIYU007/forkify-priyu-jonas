49024bb117cc2ba732b09dcca8fcfef9553925bc
PRIYU007 <technopriyu@gmail.com>
2021-04-12 01:16:52 +0530

    Test

--- src/js/controller.js
+++ src/js/controller.js
@@ -146,6 +146,7 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
+  console.log('Welcome to my app');
 };
 init();
 
